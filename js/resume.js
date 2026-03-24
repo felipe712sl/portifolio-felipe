3d0d3419ce0f640ce975b54525836e3dd3ed28e4
Felipe Louzada <felipe.louzada18@gmail.com>
2026-03-23 22:00:57 -0300

    adicionado curriculo

--- js/resume.js
+++ js/resume.js
@@ -360,6 +360,14 @@
         </div>
 
         <div class="resume__footer">
+          <a href="assets/docs/curriculo-felipe-louzada-ats.pdf" download class="resume__btn-download">
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+              <polyline points="7 10 12 15 17 10"/>
+              <line x1="12" y1="15" x2="12" y2="3"/>
+            </svg>
+            Download (ATS)
+          </a>
           <a href="assets/docs/curriculo-felipe-louzada.pdf" download class="resume__btn-download">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
               <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
@@ -382,7 +390,7 @@
   }
 
   /* ================================================
-     INICIALIZAÇÃO DO MODAL
+      INICIALIZAÇÃO DO MODAL
      ================================================ */
 
   function init() {
